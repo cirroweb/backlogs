d78aa6d6ff1e80d2700805eb0f76eeebfc28e44d
fbushell <frank@gocirro.com>
2015-01-14 21:40:00 -0700

    build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,3 @@
-// This is based on http://bit.ly/1tydiVw
-
-var EXPIRE_IN_2030 = new Date('2030');  
-var TWO_YEAR_CACHE_PERIOD_IN_SEC = 60 * 60 * 24 * 365 * 2;
-
-var deployConfig = require('./deploy.json');  
-var manifestSettings = deployConfig.manifest;
-
 module.exports = function(grunt) { 
 
   var environment = grunt.option('environment');
@@ -14,71 +6,15 @@ module.exports = function(grunt) {
     environment = 'development';
   } 
 
-  var redisConfig = deployConfig.redis[environment];
-
   grunt.initConfig({
-    aws: grunt.file.readJSON('grunt-aws.json'),
     
     shell: {
       build: {
         command: 'ember build --environment production'
+      },
+      commit: {
+        command: 'git add . && git ci -m "build" && git push origin master'
       }
-    },
-
-    upload: {
-      options: {
-        storeConfig: redisConfig
-      },
-      bootstrapIndex: {
-        options: manifestSettings,
-        files: [
-          { src: 'dist/index.html' }
-        ]
-      }
-    },
-    
-    listUploads: {
-      options: {
-        storeConfig: redisConfig
-      },
-      dist: {
-        options: manifestSettings
-      }
-    },  
-    
-    deploy: {
-      options: {
-        storeConfig: redisConfig
-      },
-      dist: {
-        options: manifestSettings
-      }
-    },
-
-    s3: {
-      options: {
-        accessKeyId: '<%= aws.key %>',
-        secretAccessKey: '<%= aws.secret %>',
-        bucket: '<%= aws.bucket %>',
-        headers: {
-          CacheControl: TWO_YEAR_CACHE_PERIOD_IN_SEC,
-          Expires: EXPIRE_IN_2030
-        },
-      },
-      build: {
-        cwd: 'dist',
-        src: 'assets/**'
-      },
-    },
-
-    git_subtree_add: {
-        subtree: {
-            options: {
-                source: "https://github.com/cirroweb/backlogs.git",
-                branch: "gh-pages",
-                target: "dist"
-            }
-        }
     },
 
     git_subtree_push: {
@@ -93,7 +29,8 @@ module.exports = function(grunt) {
 
   });
 
-  grunt.registerTask('default', ['shell', 's3:build']);
+  grunt.registerTask('default', ['shell']);
+  grunt.registerTask('doit', ['shell:build', 'shell:commit', 'git_subtree_push']);
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-aws');
   grunt.loadNpmTasks('grunt-rt-deployinator');
